8dfa15d5e5f583dc74b5564382ea7f8c5fb86a78
Tyler Cady <tylercady07@gmail.com>
2024-12-16 15:40:22 -0500

    start of a bootloader-> but not really

---
+++ bootloader/include/memory_map.h
@@ -0,0 +1,15 @@
+/* memory_map.h */
+#pragma once 
+
+extern int __bootrom_start__;
+extern int __bootrom_size__;
+extern int __aaprom_start__;
+extern int __aaprom_size__;
+extern int __iram_start__;
+extern int __iram_size__;
+extern int __dram_start__;
+extern int __dram_size__;
+extern int __rtc_sram_start__;
+extern int __rtc_sram_size__;
+
+
---
+++ bootloader/src/bootloader.c
@@ -0,0 +1,3 @@
+/* Bootloader.c */
+
+
